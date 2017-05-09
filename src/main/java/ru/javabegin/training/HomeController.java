95b47f78b9af90333511f427197a2f105826af6b
tmvf <tmvf@yandex.ru>
2017-05-09 15:32:11 +0400

    ..

--- src/main/java/ru/javabegin/training/HomeController.java
+++ src/main/java/ru/javabegin/training/HomeController.java
@@ -1,33 +1,43 @@
 package ru.javabegin.training;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
 public class HomeController {
-	
-//	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-//		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+//	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+	@RequestMapping(value = "/user", method = RequestMethod.GET)
+	public String mainPage() {
+
+		return "/content/user";
+
 	}
-	
+
+	@RequestMapping(value = "/admin", method = RequestMethod.GET)
+	public String adminPage() {
+
+		return "/content/admin";
+
+	}
+
+	@RequestMapping(value = { "/", "/login" }, method = RequestMethod.GET)
+	public ModelAndView login(@RequestParam(value = "error", required = false) String error) {
+
+		ModelAndView model = new ModelAndView();
+		if (error != null) {
+			model.addObject("error", "Invalid username or password!");
+		}
+
+		model.setViewName("login");
+
+		return model;
+
+	}
+
 }
